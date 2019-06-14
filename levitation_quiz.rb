0fa00f1d40a3bb670bd56f15ee192bdd4c6c5857
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-14 22:06:19 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,9 +1,10 @@
 
 def levitation_quiz
   ans = ""
-  while ans != ""
-	puts "What is the spell that enacts levitation?"
-	ans = gets.chomp
+  while ans != "Wingardium Leviosa"
+	  puts "What is the spell that enacts levitation?"
+	  ans = gets.chomp
+	  
 	
 end
 
